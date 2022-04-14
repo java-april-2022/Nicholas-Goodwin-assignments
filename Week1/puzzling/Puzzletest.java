d97d37bb769b5dfe1d7f954e69dedd53e26570c5
nicholasgoodwin3 <nicholasgoodwin3@gmail.com>
2022-04-13 22:13:35 -0400

    edit

---
+++ Week1/puzzling/Puzzletest.java
@@ -0,0 +1,16 @@
+package Week1.puzzling;
+import java.util.ArrayList;
+import java.util.Random;
+public class Puzzletest {
+	public static void main(String[] args) {
+		PuzzleJava generator = new PuzzleJava();
+		ArrayList<Integer> randomRolls = generator.getTenRolls();
+		System.out.println(randomRolls);
+		
+    	//..
+		// Write your other test cases here.
+		//..
+	}
+}
+
+
---
+++ Week1/puzzling/Puzzleutil.java
@@ -0,0 +1,5 @@
+package Week1.puzzling;
+
+public class Puzzleutil {
+    
+}
